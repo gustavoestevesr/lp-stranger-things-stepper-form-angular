9a00085c955a08f929864b72db2cbc15562473d5
gustavoesteves <gustavoestevesr@directon.com.br>
2024-06-04 18:53:53 -0300

    feat: utilização dos componentes e troca de tema dark x light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'lp-stranger-things-stepper-form-angular';
+
+  darkTheme = false; // Defina true para tema escuro e false para tema claro
+  @ViewChild('bodyElement', { static: true }) bodyElement!: ElementRef;
+
+  subscribe() {}
+
+  toggleThemeReceiver(darkTheme: boolean) {
+    this.darkTheme = darkTheme;
+  }
 }
